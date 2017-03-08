11e8b1627b17c92c65c37737e424a242b3280e44
151250176 <151250176@smail.nju.edu.cn>
2017-03-08 10:23:49 +0800

    feat: add json string to javaPO

---
+++ MRClientModule/src/main/java/data/GsonUtil.java
@@ -0,0 +1,45 @@
+package data;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by SilverNarcissus on 2017/3/8.
+ */
+public class GsonUtil {
+    /**
+     * 用于解析的gson实例
+     */
+    private static final Gson gson = new Gson();
+
+    /**
+     * 将Json数据解析成相应的映射对象
+     *
+     * @param jsonData 需要被解析的json串
+     * @param type     要生成的PO.class
+     * @param <T>      要生成的PO.class
+     * @return 解析得到的PO
+     */
+    public static <T> T parseJson(String jsonData, Class<T> type) {
+        return gson.fromJson(jsonData, type);
+    }
+
+    /**
+     * 将Json数组解析成相应的映射对象列表
+     *
+     * @param jsonData 需要被解析的json串
+     * @param type     要生成的PO.class
+     * @param <T>      要生成的PO.class
+     * @return 解析得到的PO列表
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> List<T> paeseJsonAsList(String jsonData, Class<T[]> type)
+    {
+        Gson gson = new Gson();
+        T[] array = gson.fromJson(jsonData, type);
+        return Arrays.asList(array);
+    }
+}
---
+++ MRClientModule/src/main/java/data/ReviewDataFromJsonServiceImpl.java
@@ -0,0 +1,9 @@
+package data;
+
+/**
+ * Created by SilverNarcissus on 2017/3/8.
+ */
+public class ReviewDataFromJsonServiceImpl {
+
+
+}
