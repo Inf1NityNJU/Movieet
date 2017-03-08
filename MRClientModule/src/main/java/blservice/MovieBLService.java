b3e8d2b6cacc46dc905c7044adbafb7392e02ca5
141099096 <141099096@smail.nju.edu.cn>
2017-03-08 09:33:51 +0800

    dev:update blservice

--- MRClientModule/src/main/java/blservice/MovieBLService.java
+++ MRClientModule/src/main/java/blservice/MovieBLService.java
@@ -34,16 +34,18 @@ public interface MovieBLService {
     /**
      * 根据电影 id 查找每月评论数量
      * @param movieId
+     * @param startMonth eg. 2017-01
+     * @param endMonth eg. 2017-03
      * @return
      */
-    public ReviewCountMonthVO  findMonthCountByMovieId(String movieId);
+    public ReviewCountMonthVO  findMonthCountByMovieId(String movieId, String startMonth, String endMonth);
 
 
     /**
      * 根据电影 id 和起始时间和结束时间查找每日评论数量
      * @param movieId
-     * @param startDate
-     * @param endDate
+     * @param startDate eg. 2017-01-12
+     * @param endDate eg. 2017-02-03
      * @return
      */
     public ReviewCountMonthVO  findDayCountByMovieId(String movieId, String startDate, String endDate);
