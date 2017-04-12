9ac90ae4c16e521d528fc4793a75e15b418368eb
151250182 <151250182@smail.nju.edu.cn>
2017-04-12 15:09:26 +0800

    feat<bl>: update moviegenre

--- MRClientModule/src/main/java/util/MovieGenre.java
+++ MRClientModule/src/main/java/util/MovieGenre.java
@@ -3,41 +3,31 @@ package util;
 /**
  * Created by vivian on 2017/3/27.
  * 电影分类
- * 以下分类来源于Amazon网站
+ * 以下分类来源于IMDB中已经出现的数据
  */
 public enum MovieGenre {
-    ALL,
-    ACTION,
-    ADVENTURE,
-    ANIMATION, //动画片
-    ANIME, //日本动画
-    ART,
-    BOXED_SETS, //BOXED系列
-    CLASSICS,
-    COMEDY,
-    DOCUMENTARY,
-    DRAMA,
-    EDUCATIONAL,
-    EXERCISE,
-    FITNESS,
-    FAITH,
-    SPIRITUALITY,
-    FANTASY,
-    HOLIDAY,
-    SEASONAL,
-    HORROR,
-    INDIE, //非主流或非商业目的的电影
-    KIDS,
-    FAMILY,
-    MILITARY,
-    WAR,
-    MUSIC,
-    CONCERTS,
-    MYSTERY,
-    THRILLERS,
-    ROMANCE,
-    SCIENCE,
-    FICTION;
+    NA,  //没有分类
+    Action ,
+    Adventure ,
+    Horror ,
+    Romance ,
+    War ,
+    History ,
+    Documentary ,
+    SciFi ,
+    Sport ,
+    Drama ,
+    Thriller ,
+    Music ,
+    Crime ,
+    Fantasy ,
+    Biography ,
+    Animation ,
+    Family ,
+    Mystery ,
+    Comedy ,
+    Musical ,
+    Short;
 
     public String getGenreName() {
         return toString().charAt(0) + toString().toLowerCase().substring(1);
