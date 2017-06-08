50e79db834fccecdb3cb519b6de6dc93660c2f8c
141099096 <141099096@smail.nju.edu.cn>
2017-06-08 21:03:12 +0800

    dev<ui>:add user similar recommend

--- MRWebModule/src/main/webapp/react/src/services/user.js
+++ MRWebModule/src/main/webapp/react/src/services/user.js
@@ -1,110 +1,120 @@
-import request, { requestWithoutError } from '../utils/request';
+import request, {requestWithoutError} from '../utils/request';
 
-import {PREVIEW_COLLECT_SIZE, PREVIEW_EVALUATE_SIZE, PREVIEW_FRIEND_SIZE } from '../constants'
+import {PREVIEW_COLLECT_SIZE, PREVIEW_EVALUATE_SIZE, RECOMMEND_SIZE} from '../constants'
 
 export function fetch() {
-  return request(`/api/user`, {
-    method: 'GET',
-    headers: {
-      'Authorization': localStorage.getItem('token')
-    },
-  });
+    return request(`/api/user`, {
+        method: 'GET',
+        headers: {
+            'Authorization': localStorage.getItem('token')
+        },
+    });
 }
 
 export function fetchUser(id) {
-  const url = `/api/user/info/${id}`;
-  return request(url, {
-    method: 'GET',
-  });
+    const url = `/api/user/info/${id}`;
+    return request(url, {
+        method: 'GET',
+    });
 }
 
 export function fetchUserFollow(id) {
-  const url = `/api/user/${id}/follow`;
-  return request(url, {
-    method: 'GET',
-    headers: {
-      'Authorization': localStorage.getItem('token')
-    },
-  });
+    const url = `/api/user/${id}/follow`;
+    return request(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': localStorage.getItem('token')
+        },
+    });
+}
+
+export function fetchUserSimilarity(id) {
+    const url = `/api/user/${id}/similarity`;
+    return request(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': localStorage.getItem('token')
+        },
+    });
 }
 
 export function signUp(user) {
-  return request(`/api/user/signup`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(user),
-  });
+    return request(`/api/user/signup`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user),
+    });
 }
 
 export function signIn(user) {
-  return requestWithoutError(`/api/user/signin`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(user),
-  });
+    return requestWithoutError(`/api/user/signin`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user),
+    });
 }
 
 export function signOut() {
-  localStorage.removeItem('token');
-  return request(`/api/user/signout`, {
-    method: 'POST',
-  });
+    localStorage.removeItem('token');
+    return request(`/api/user/signout`, {
+        method: 'POST',
+    });
 }
 
 export function postUserSurvey(survey) {
-  const url = `api/user/survey`;
-  return request(url, {
-    method: 'POST',
-    headers: {
-      'Authorization': localStorage.getItem('token')
-    },
-    body: JSON.stringify(survey),
-  });
+    const url = `api/user/survey`;
+    return request(url, {
+        method: 'POST',
+        headers: {
+            'Authorization': localStorage.getItem('token')
+        },
+        body: JSON.stringify(survey),
+    });
 }
 
 
 export function fetchUserCollectMovies(id, size = PREVIEW_COLLECT_SIZE, page = 1) {
-  const url = `api/user/${id}/collect?orderBy=time&order=desc&size=${size}&page=${page}`;
-  return request(url, {
-    method: 'GET',
-    headers: {
-      'Authorization': localStorage.getItem('token')
-    },
-  });
+    const url = `api/user/${id}/collect?orderBy=time&order=desc&size=${size}&page=${page}`;
+    return request(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': localStorage.getItem('token')
+        },
+    });
 }
 
 export function fetchUserEvaluateMovies(id, size = PREVIEW_EVALUATE_SIZE, page = 1) {
-  const url = `api/user/${id}/evaluate?orderBy=time&order=desc&size=${size}&page=${page}`;
-  return request(url, {
-    method: 'GET',
-    headers: {
-      'Authorization': localStorage.getItem('token')
-    },
-  });
+    const url = `api/user/${id}/evaluate?orderBy=time&order=desc&size=${size}&page=${page}`;
+    return request(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': localStorage.getItem('token')
+        },
+    });
 }
 
 export function fetchUserFollowing(id, size = PREVIEW_COLLECT_SIZE, page = 1) {
-  const url = `api/user/${id}/following?orderBy=time&order=desc&size=${size}&page=${page}`;
-  return request(url, {
-    method: 'GET',
-    headers: {
-      'Authorization': localStorage.getItem('token')
-    },
-  });
+    const url = `api/user/${id}/following?orderBy=time&order=desc&size=${size}&page=${page}`;
+    return request(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': localStorage.getItem('token')
+        },
+    });
 }
 
 export function fetchUserFollower(id, size = PREVIEW_COLLECT_SIZE, page = 1) {
-  const url = `api/user/${id}/follower?orderBy=time&order=desc&size=${size}&page=${page}`;
-  return request(url, {
-    method: 'GET',
-    headers: {
-      'Authorization': localStorage.getItem('token')
-    },
-  });
+    const url = `api/user/${id}/follower?orderBy=time&order=desc&size=${size}&page=${page}`;
+    return request(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': localStorage.getItem('token')
+        },
+    });
 }
 
 export function followUser(id) {
@@ -121,6 +131,16 @@ export function unfollowUser(id) {
     const url = `api/user/${id}/follow`;
     return request(url, {
         method: 'DELETE',
+        headers: {
+            'Authorization': localStorage.getItem('token')
+        },
+    });
+}
+
+export function fetchUserRecommend(size = RECOMMEND_SIZE) {
+    const url = `api/user/similar/recommend?size=${size}`;
+    return request(url, {
+        method: 'GET',
         headers: {
             'Authorization': localStorage.getItem('token')
         },
